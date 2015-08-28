df4a255e6543eca484adde84e8847ac99c633618
Rahendra Putra K <rahendz@gmail.com>
2015-08-28 14:39:35 +0700

    edit index.php

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'tofa';
+	$system_path = 'system';
 
 /*
  *---------------------------------------------------------------
