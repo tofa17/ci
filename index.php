d6ef91dc4f1755ddf966f159be5db5c62c503119
tofa17 <tofariyadi17@gmail.com>
2015-08-28 14:50:21 +0700

    fhja

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'telo';
+	$application_folder = 'application';
 
 /*
  *---------------------------------------------------------------
